ff8abf9c9a8b69f19ef99467e86ddd108d84212f
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-09 13:35:18 +0300

    Добавил буффер вывода в класс View

--- classes/View.php
+++ classes/View.php
@@ -14,13 +14,22 @@ class View
     {
         $this->data[$name] = $value;
     }
-
-    public function display($template)
+    public function __set($k, $v){
+        $this->data[$k]=$v;
+    }
+    public function render($template)
     {
         foreach ($this->data as $key=>$val){
             $$key = $val;
         }
+        ob_start();
         include __DIR__.'/../views/'.$template;
+        $content = ob_get_contents();
+        ob_end_clean();
+        return $content;
     }
 
+    public function display($template){
+        echo $this->render($template);
+    }
 }
