c56a5aa9db645fff4c783da76727c338dc108f98
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-10 18:28:44 +0300

    Добавил модели

---
+++ models/NewsModel.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 09.08.15
+ * Time: 15:29
+ */
+class NewsModel extends AbstractModel
+{
+    static protected $table = 'news';
+
+    /*public $id;
+    public $title;
+    public $text;
+*/
+    }
