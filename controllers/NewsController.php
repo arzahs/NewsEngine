3a915af034d69032ac3080d49f81b4316d762be4
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-10 18:23:29 +0300

    Добавил .htacces

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -6,33 +6,16 @@ class NewsController
     public function actionAll()
     {
 
-        $art = NewsModel::findOneByColumn('title', 'New header1');
-        $art->title = 'New header';
-        $art->save();
-       /* $art = new NewsModel();
-        $art->title = 'Today in world';
-        $art->text = 'ololdfvdsvsd';
-        $art->insert();
-
-        var_dump($art->id);
-       /* $article = new NewsModel();
-        $article->title = 'Привет!';
-        $article->text = 'Привет world!';*/
-
-        //var_dump( isset($article->title) );
-
-        /*$article->insert();*/
-
-        /*$items = News::ge tAll();
+        $items = NewsModel::findAll();
         $view = new View();
         $view->assign('items', $items);
-        $view->display('news/all.php');*/
+        $view->display('news/all.php');
 
     }
     public function actionOne()
     {
         $id = $_GET['id'];
-        $item = News::getOne($id);
+        $item = NewsModel ::findOne($id);
         $view = new View();
         $view->assign('item', $item);
         $view->display('news/one.php');
