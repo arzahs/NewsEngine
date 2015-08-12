20e618343e66785f078ccfca0f113a41d3596097
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-12 13:39:13 +0300

    В фронт контроллере закоментирована логика работы с .htaccess

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
 
 require_once __DIR__.'/autoload.php';
+
+$path=parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$pathParts = explode('/', $path);
+var_dump($pathParts);
+
 $ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
+/*$ctrl = !empty($pathParts[1]) ? $pathParts[1] : 'News';
+$act = !empty($pathParts[2]) ? $pathParts[2] : 'All';
+*/
 $controllerClassName = $ctrl.'Controller';
 
 try{
