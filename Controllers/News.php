b52e24a7b4e6b5f653b53e52d100253a72052c26
Sergey Nelepa <nelepa1995@mail.ru>
2015-08-12 18:36:03 +0300

    Изменил название Моделей и Контроллера

---
+++ Controllers/News.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Controllers;
+
+use Application\Models\News as NewsModel;
+
+class News
+{
+    public function actionAll()
+    {
+
+        $items = NewsModel::findAll();
+        $view = new \View();
+        $view->assign('items', $items);
+        $view->display('news/all.php');
+
+    }
+    public function actionOne()
+    {
+        $id = $_GET['id'];
+        $item = NewsModel::findOne($id);
+        $view = new \View();
+        $view->assign('item', $item);
+        $view->display('news/one.php');
+    }
+}
---
+++ Models/News.php
@@ -0,0 +1,17 @@
+<?php
+namespace Application\Models;
+/**
+ * Created by PhpStorm.
+ * User: sergey
+ * Date: 09.08.15
+ * Time: 15:29
+ */
+class News extends \AbstractModel
+{
+    static protected $table = 'news';
+
+    /*public $id;
+    public $title;
+    public $text;
+*/
+    }
